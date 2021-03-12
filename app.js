41db5e9e16b034049db4b39c6920fa38e511de46
Mirabror Gulomov <00010123@wiut.uz>
2021-03-12 17:57:15 +0500

    add some new pages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express  = require ('express')
 const app =  express()
 
+const notes = [
+	'Some awesome note-1',
+	'Some awesome note-2',
+	'Some awesome note-3'
+]
+
 app.use('/static', express.static('public'))
 
 app.set('view engine', 'pug')
@@ -8,6 +14,17 @@ app.set('view engine', 'pug')
 app.get('/', (req, res) => {
 	res.render('home')
 })
+app.get('/create', (req, res) => {
+	res.render('create')
+})
+app.get('/notes', (req, res) => {
+	res.render('notes', {notes:notes})
+})
+app.get('/notes/1', (req, res) => {
+	res.render('detail', {notes:notes})
+})
+
+
 
 
 app.listen(8000, err => {
